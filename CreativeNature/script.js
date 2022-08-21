59fc51874ca5b35a657804ca9886e40395593537
crudEverySingleDay( )； <XINLEROY.LIU001@STU.BMCC.CUNY.EDU>
2022-08-22 00:13:05 +0800

    Upload: CreativeNature

---
+++ CreativeNature/script.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.content', { delay: 200,duration: 3000 });
+ScrollReveal().reveal('.header', { delay: 200,duration: 3000 });
+ScrollReveal().reveal('.media-icons', { delay: 200,duration: 3000 });
